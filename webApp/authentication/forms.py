feddda6d52a49ff0b348361266c7ea75b49fb0cf
Alexandre WALLOIS <a.wallois@resonance-advisory.com>
2024-07-02 19:25:47 +0200

    remove files

--- webApp/authentication/forms.py
+++
@@ -1,6 +0,0 @@
-# authentication/forms.py
-from django import forms
-
-class LoginForm(forms.Form):
-    email = forms.CharField(max_length=63, widget=forms.EmailInput, label='Email')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
